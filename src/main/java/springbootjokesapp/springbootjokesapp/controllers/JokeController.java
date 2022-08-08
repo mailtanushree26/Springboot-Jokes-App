c4ef4c8627e15ecd11c63a8eefc72c79b015a084
Tanu Shree Sharma <mailtanushree26@gmail.com>
2022-08-09 00:48:33 +0530

    Created Joke Controller and model in it and also done mapping

---
+++ src/main/java/springbootjokesapp/springbootjokesapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package springbootjokesapp.springbootjokesapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springbootjokesapp.springbootjokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+@RequestMapping({"/", " "})
+    public String showJoke (Model model)
+    {
+        model.addAttribute("Joke", jokeService.getJoke());
+        return "index";
+    }
+
+
+}
